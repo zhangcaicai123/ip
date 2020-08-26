9f4c73bcf79086f226442695218405917a33222c
zhangcaicai123 <43933817+zhangcaicai123@users.noreply.github.com>
2020-08-26 16:47:29 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,20 +6,40 @@ public class Duke {
         System.out.println("     What can I do for you?");
         System.out.println("    ____________________________________________________________");
         String line;
+        int itemCount = 0;
+        String [] list = new String[100];
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while(!line.equals("bye")){
+            if(line.equals("list")) printList(list,itemCount);
+            else{
             echo(line);
+            list[itemCount] = line;
+            itemCount++;
+            }
             line = in.nextLine();
         }
-        System.out.println("    ____________________________________________________________");
-        System.out.println("     Bye. Hope to see you again soon!");
-        System.out.println("    ____________________________________________________________");
+        bye();
+
 
     }
     public static void echo(String line){
         System.out.println("    ____________________________________________________________");
-        System.out.println("     "+line);
+        System.out.println("     added: "+line);
+        System.out.println("    ____________________________________________________________");
+    }
+    public static void bye(){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+    public static void printList(String[] list,int itemCount){
+        System.out.println("    ____________________________________________________________");
+        for (int i=0;i<itemCount;i++){
+            System.out.print("     ");
+            System.out.print(i+1);
+            System.out.println(". "+list[i]);
+        }
         System.out.println("    ____________________________________________________________");
     }
 }
