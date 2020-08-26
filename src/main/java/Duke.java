a0faa90a44105c938346f8abb538dbbd88d5897d
zhangcaicai123 <43933817+zhangcaicai123@users.noreply.github.com>
2020-08-26 16:25:18 +0800

    Level - 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello！ I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello！ I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals("bye")){
+            echo(line);
+            line = in.nextLine();
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
 
+    }
+    public static void echo(String line){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     "+line);
+        System.out.println("    ____________________________________________________________");
     }
 }
